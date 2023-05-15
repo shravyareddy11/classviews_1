6284db58f87de2dd62374934d6c5f2ee78e0d252
shravyareddy11 <shravyareddyg915@gmail.com.com>
2023-05-15 18:03:05 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from typing import Any, Dict
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+class cbv_context(TemplateView):
+    template_name='cbv_context.html'
+
+    def get_context_data(self, **kwargs) :
+        EDCO=super().get_context_data(**kwargs)
+        
+        EDCO['name']="shravya"
+        EDCO['age']=21
+        return EDCO
+
+
+
+
+
+
+
